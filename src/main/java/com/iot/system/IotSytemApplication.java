277d065191d91f3b4b0f46e8348f17749459e6a4
Rodolfo Silva <rodolfo.diego.gomes@gmail.com>
2024-06-05 20:43:56 -0400

    Initial project

---
+++ src/main/java/com/iot/system/IotSytemApplication.java
@@ -0,0 +1,13 @@
+package com.iot.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotSytemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotSytemApplication.class, args);
+	}
+
+}
