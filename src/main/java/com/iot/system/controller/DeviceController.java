92d6d4f0b5a34f08d5dc07328bc67f2555338ec1
Rodolfo Silva <rodolfo.diego.gomes@gmail.com>
2024-06-05 22:02:13 -0400

    Add Swagger api doc

--- src/main/java/com/iot/system/controller/DeviceController.java
+++ src/main/java/com/iot/system/controller/DeviceController.java
@@ -2,6 +2,9 @@ package com.iot.system.controller;
 
 import com.iot.system.model.Device;
 import com.iot.system.service.DeviceService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,17 +14,22 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/devices")
+@Tag(name = "Device", description = "API for managing IoT devices")
 public class DeviceController {
     @Autowired
     private DeviceService deviceService;
 
     @GetMapping
+    @Operation(summary = "Get all devices", description = "Retrieve a list of all devices")
     public List<Device> getAllDevices() {
         return deviceService.getAllDevices();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Device> getDeviceById(@PathVariable Long id) {
+    @Operation(summary = "Get a device by ID", description = "Retrieve a device by its ID")
+    public ResponseEntity<Device> getDeviceById(
+            @Parameter(description = "ID of the device to be retrieved", required = true)
+            @PathVariable Long id) {
         Device device = deviceService.getDeviceById(id);
         if (device == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -30,12 +38,17 @@ public class DeviceController {
     }
 
     @PostMapping
+    @Operation(summary = "Add a new device", description = "Add a new device to the system")
     public Device createDevice(@RequestBody Device device) {
         return deviceService.saveDevice(device);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Device> updateDevice(@PathVariable Long id, @RequestBody Device deviceDetails) {
+    @Operation(summary = "Update a device", description = "Update an existing device")
+    public ResponseEntity<Device> updateDevice(
+            @Parameter(description = "ID of the device to be updated", required = true)
+            @PathVariable Long id,
+            @RequestBody Device deviceDetails) {
         Device updatedDevice = deviceService.updateDevice(id, deviceDetails);
         if (updatedDevice == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -44,7 +57,10 @@ public class DeviceController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteDevice(@PathVariable Long id) {
+    @Operation(summary = "Delete a device", description = "Delete a device from the system")
+    public ResponseEntity<Void> deleteDevice(
+            @Parameter(description = "ID of the device to be deleted", required = true)
+            @PathVariable Long id) {
         deviceService.deleteDevice(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
